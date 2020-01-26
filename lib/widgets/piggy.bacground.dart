1eca49d3e006c1bfb28fd2d48d272c367d3dec25
Unknown <szilagyi.adam4@gmail.com>
2020-01-26 22:01:59 +0100

    ui fix

--- lib/widgets/piggy.bacground.dart
+++ lib/widgets/piggy.bacground.dart
@@ -6,12 +6,13 @@ piggyBackgroundDecoration(BuildContext context) {
     image: DecorationImage(
         image: AssetImage(
           'assets/images/adult_profile_4K.png',
+
         ),
         fit: BoxFit.cover,
         colorFilter: ColorFilter.mode(
             Color.fromRGBO(255, 255, 255, 0.6), BlendMode.lighten),
         alignment: AlignmentDirectional(
-            MediaQuery.of(context).size.width * 0.0014, 0)),
+            MediaQuery.of(context).size.width * 0.001 - 0.1, 0)),
   );
 }
 
